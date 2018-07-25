bde30029f0bf1ac563e880503b74592cdd03b46f
Andrew Briz <briz.andrew@gmail.com>
2018-07-25 15:56:05 -0400

    Updated services path.

--- politicoservicesapi/urls.py
+++ politicoservicesapi/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(
-        r'^oembed/',
+        r'^api/services/oembed/',
         include('oembedservice.urls', namespace='oembedservice')
     ),
     url(
-        r'^s3imageservice/',
+        r'^api/services/s3imageservice/',
         include('s3imageservice.urls', namespace='s3imageservice')
     ),
 ]
