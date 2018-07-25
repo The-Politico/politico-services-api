51beee8b563e463cc38d520785f8110dc58f4bbb
Andrew Briz <briz.andrew@gmail.com>
2018-07-25 17:11:40 -0400

    That's in there twice.

--- politicoservicesapi/settings.py
+++ politicoservicesapi/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
