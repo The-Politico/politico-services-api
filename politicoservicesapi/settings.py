3af6513fbc0173bf1e86c78975ef1fc26139c944
Jon McClure <jmcclure@politico.com>
2018-12-03 13:25:08 -0500

    adds cors regex for localhost access

--- politicoservicesapi/settings.py
+++ politicoservicesapi/settings.py
@@ -3,101 +3,101 @@ import os
 import environ
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-env = environ.Env(DEBUG=(bool, False),)
-environ.Env.read_env('{0}/.env'.format(BASE_DIR))
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env("{0}/.env".format(BASE_DIR))
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
+SECRET_KEY = os.getenv("SECRET_KEY", "secret")
 
-DEBUG = True if os.getenv('DEBUG', False) else False
+DEBUG = True if os.getenv("DEBUG", False) else False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'rest_framework',
-    'storages',
-    's3imageservice',
-    'oembedservice',
-    'tokenservice',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework",
+    "storages",
+    "s3imageservice",
+    "oembedservice",
+    "tokenservice",
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'politicoservicesapi.urls'
+ROOT_URLCONF = "politicoservicesapi.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'politicoservicesapi.wsgi.application'
+WSGI_APPLICATION = "politicoservicesapi.wsgi.application"
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
 
 DATABASES = {}
-if 'DATABASE_URL' in os.environ:  # noqa: F821
-    DATABASES['default'] = env.db()
+if "DATABASE_URL" in os.environ:  # noqa: F821
+    DATABASES["default"] = env.db()
 else:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME', None),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', None),
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'PORT': os.environ.get('POSTGRES_PORT', 5432),
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_NAME", None),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", None),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", 5432),
     }
 
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"  # noqa
     },
 ]
 
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -107,10 +107,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-WHITENOISE_STATIC_PREFIX = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'politicoservicesapi', 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = "/static/"
+WHITENOISE_STATIC_PREFIX = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "politicoservicesapi", "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ########
 # CORS #
@@ -118,31 +118,27 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 if DEBUG:
     CORS_ORIGIN_ALLOW_ALL = True
 else:
-    CORS_ORIGIN_WHITELIST = (
-        'localhost:8000',
-        'localhost:3000',
-        'the-politico.github.io',
-        'datalab.politico.com',
-    )
+    CORS_ORIGIN_WHITELIST = ("the-politico.github.io", "datalab.politico.com")
+    CORS_ORIGIN_REGEX_WHITELIST = (r"localhost:[0-9]{4}",)
 
 ###########################
 # s3 image service settings
 S3IMAGESERVICE_API_AUTHENTICATION_CLASS = (
-    'tokenservice.authentication.TokenAuthentication'
+    "tokenservice.authentication.TokenAuthentication"
 )
 
 S3IMAGESERVICE_FILE_MB_LIMIT = 100
-S3IMAGESERVICE_S3_UPLOAD_ROOT = 'interactives/uploads/image-service/'
-S3IMAGESERVICE_MEDIA_PATH = 'image-service/'
-S3IMAGESERVICE_AWS_REGION = 'us-east-2'
+S3IMAGESERVICE_S3_UPLOAD_ROOT = "interactives/uploads/image-service/"
+S3IMAGESERVICE_MEDIA_PATH = "image-service/"
+S3IMAGESERVICE_AWS_REGION = "us-east-2"
 
-S3IMAGESERVICE_AWS_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY_ID')
-S3IMAGESERVICE_AWS_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY')
-S3IMAGESERVICE_AWS_S3_BUCKET = os.getenv('AWS_S3_PUBLISH_BUCKET')
-S3IMAGESERVICE_AWS_S3_STATIC_ROOT = os.getenv('AWS_S3_STATIC_ROOT')
+S3IMAGESERVICE_AWS_ACCESS_KEY_ID = os.getenv("AWS_S3_ACCESS_KEY_ID")
+S3IMAGESERVICE_AWS_SECRET_ACCESS_KEY = os.getenv("AWS_S3_SECRET_ACCESS_KEY")
+S3IMAGESERVICE_AWS_S3_BUCKET = os.getenv("AWS_S3_PUBLISH_BUCKET")
+S3IMAGESERVICE_AWS_S3_STATIC_ROOT = os.getenv("AWS_S3_STATIC_ROOT")
 
 #########################
 # oembed service settings
 OEMBEDSERVICE_API_AUTHENTICATION_CLASS = (
-    'tokenservice.authentication.TokenAuthentication'
+    "tokenservice.authentication.TokenAuthentication"
 )
